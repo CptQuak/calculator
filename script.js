a317934022c21e57e38853a5e1ab55a7246e9e6a
Quak <kamilkulesza5@gmail.com>
2021-10-09 14:58:47 +0200

    basic functionality done i think

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function divide(a, b) {
         return round(a / b, 3);
     }
 }
-
 function operate(operator, a, b) {
     switch (operator) {
         case "+":
@@ -38,6 +37,43 @@ function operate(operator, a, b) {
     }
 }
 
+function updateScreen(buttonValue) {
+    //empty screen and dot pressed start wrigin float number
+    if (buttonValue == "." && screen[0].textContent == 0) {
+        screen[0].textContent = "0.";
+    } //updating screen for float number that is starting with 0
+    else if (
+        screen[0].textContent[0] == "0" &&
+        screen[0].textContent[1] == "."
+    ) {
+        screen[0].textContent += `${buttonValue}`;
+    } //entering value greater than 0
+    else if (screen[0].textContent == 0) {
+        screen[0].textContent = buttonValue;
+    } //updating screen with next numbers if first entered number is not 0
+    else {
+        screen[0].textContent += +`${buttonValue}`;
+    }
+    displayValue = screen[0].textContent;
+}
+
+function clearScreen(buttonValue) {
+    if (buttonValue == "C") {
+        resetCalculator();
+    } else {
+        hiddenValue = displayValue;
+        currentOperator = buttonValue;
+        screen[0].textContent = 0;
+        displayValue = screen[0].textContent;
+    }
+}
+
+function resetCalculator() {
+    displayValue = 0;
+    hiddenValue = 0;
+    currentOperator = 0;
+    screen[0].textContent = 0;
+}
 // console.log(operate("+", 2, 2));
 // console.log(operate("-", 6, 3));
 // console.log(operate("*", 2, 7));
@@ -45,3 +81,70 @@ function operate(operator, a, b) {
 // console.log(operate("/", 2, 0));
 
 let displayValue = 0;
+let hiddenValue = 0;
+let currentOperator = "";
+const numericButtons = [];
+const operatorButtons = [];
+const actionButtons = [];
+const screen = document.querySelectorAll(".screentext");
+//segregating buttons for later use
+const buttonHolder = document.querySelectorAll(".button");
+
+buttonHolder.forEach((button) => {
+    switch (button.textContent) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            numericButtons.push(button);
+            break;
+        case "+":
+        case "*":
+        case "/":
+        case "-":
+            // case "sqr":
+            // case "%":
+            operatorButtons.push(button);
+            break;
+        case "=":
+        case "C":
+        case ".":
+            actionButtons.push(button);
+            break;
+    }
+});
+//on every numeric button click update screen value
+numericButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        updateScreen(button.textContent);
+    });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        clearScreen(button.textContent);
+    });
+});
+
+actionButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.textContent == "=" && !currentOperator == "") {
+            screen[0].textContent = operate(
+                currentOperator,
+                parseFloat(hiddenValue),
+                parseFloat(displayValue)
+            );
+            displayValue = screen[0].textContent;
+        } else if (button.textContent == ".") {
+            updateScreen(button.textContent);
+        } else if (button.textContent == "C") {
+            clearScreen(button.textContent);
+        }
+    });
+});
